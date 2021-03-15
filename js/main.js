9042d27ecb642f8d91a89583a8d1509c374481eb
AlexBor83 <rabota_for_alex@mail.ru>
2021-03-15 21:34:47 +0400

    Исправил ошибки в модуле  6.1

--- js/main.js
+++ js/main.js
@@ -3,5 +3,5 @@ import './util.js';
 import './data.js';
 import './generate-form.js';
 import './check-option-form.js';
-//import './create-map.js';
-import './page-off.js';
+import './activation-map.js';
+import './page.js';
---
+++ js/page.js
@@ -0,0 +1,41 @@
+
+const form = document.querySelector('.ad-form');
+const pageElements = document.querySelectorAll('.ad-form__element');
+const mapFilter = document.querySelector('.map__filters');
+const mapFilters = document.querySelectorAll('.map__filter');
+
+
+
+const deactivationPage = () => {
+  form.classList.add('ad-form--disabled');
+  document.querySelector('.ad-form-header').disabled=true;
+  pageElements.forEach((Element) => {
+    Element.disabled=true;
+  });
+
+  mapFilter.classList.add('map__filters--disabled');
+  mapFilters.forEach((filter) => {
+    filter.disabled=true;
+  });
+};
+
+//Страница неактивна
+deactivationPage()
+
+const activationPage = () => {
+  form.classList.remove('ad-form--disabled');
+  document.querySelector('.ad-form-header').disabled= false;
+  pageElements.forEach((Element) => {
+    Element.disabled= false;
+  });
+
+  mapFilter.classList.remove('map__filters--disabled');
+  mapFilters.forEach((filter) => {
+    filter.disabled=false;
+  });
+};
+
+//Страница активна
+//activationPage();
+
+export {activationPage};
