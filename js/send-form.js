9577c654829377df94426bfbaa4e0a9cbd897cbd
AlexBor83 <rabota_for_alex@mail.ru>
2021-03-25 00:59:26 +0400

    Поправил отпрвку формы

--- js/send-form.js
+++ js/send-form.js
@@ -12,6 +12,25 @@ const formTemplateError = document.querySelector('#error').content;
 const formError = formTemplateError.querySelector('.error');
 const formErrorButton = formTemplateError.querySelector('.error__button');
 
+const onPopupSuccessEscKeydown = (evt) => {
+  if (isEscEvent(evt)) {
+    evt.preventDefault();
+    formSuccess.classList.add('hidden');
+  }
+};
+
+const onPopupErrorEscKeydown = (evt) => {
+  if (isEscEvent(evt)) {
+    evt.preventDefault();
+    formError.classList.add('hidden');
+  }
+};
+
+main.appendChild(formTemplateError);
+main.appendChild(formTemplateSuccess);
+formSuccess.classList.add('hidden');
+formError.classList.add('hidden');
+
 const clearInputs = () => {
   inputs.forEach((item) =>{
     item.value = '';
@@ -19,43 +38,30 @@ const clearInputs = () => {
 };
 
 const onSuccess = () => {
-  main.appendChild(formTemplateSuccess);
+  formSuccess.classList.remove('hidden')
   main.addEventListener ('click', () => {
     formSuccess.classList.add('hidden');
   });
 
-  document.addEventListener('keydown', (evt) => {
-    if (isEscEvent(evt)) {
-      evt.preventDefault();
-      formSuccess.classList.add('hidden');
-    }
-  });
+  document.addEventListener('keydown', onPopupSuccessEscKeydown);
   clearInputs();
 
   mainMarker.setLatLng(TOKIO_LAT_LNG);
-
-  formSuccess.classList.remove('hidden');
-
 };
 
 const onError = () => {
-  main.appendChild(formTemplateError);
+  formError.classList.remove('hidden');
   formErrorButton.addEventListener('click', () => {
     formError.classList.add('hidden');
   });
 
-  document.addEventListener('keydown', (evt) => {
-    if (isEscEvent(evt)) {
-      evt.preventDefault();
-      formError.classList.add('hidden');
-    }
-  });
-
-  formError.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupErrorEscKeydown);
 };
 
 form.addEventListener('submit', (evt) => {
   evt.preventDefault();
   const formData = new FormData(evt.target);
   send(formData, onSuccess, onError);
+  document.removeEventListener('keydown', onPopupSuccessEscKeydown);
+  document.removeEventListener('keydown', onPopupErrorEscKeydown);
 });
